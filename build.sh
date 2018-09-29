078db556cfbeb5bc7bf9dea82a41ddd0f392ccd1
Jesse Patching <jesse@patching.ca>
2018-09-29 10:20:15 -0600

    Added build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REPO_NAME=radpenguin/rclone-sync
+
+BUILD_DATE=$( date +"%Y-%m-%d-%H-%M-%S" )
+VERSION=$( curl --silent https://rclone.org/downloads/ | grep "Rclone Download" | sed -e 's/^.*Rclone Download v//' )
+
+echo "Building $REPO_NAME version $VERSION, build date $BUILD_DATE"
+echo -n "Continue? (y/N) "
+read X
+if [[ "$X" != "y" ]]; then
+  echo "Exiting..."
+  exit 1
+fi
+
+docker build \
+  --no-cache \
+  --build-arg=BUILD_DATE="$BUILD_DATE" \
+  --build-arg=VERSION="$VERSION" \
+  --tag=$REPO_NAME \
+  .
+
+docker push $REPO_NAME
